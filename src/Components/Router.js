1517004656e14a33d1876f280f4c8f6628c638b9
ParkSoHyeon <dso753@naver.com>
2021-03-31 21:43:31 +0900

    React Router

---
+++ src/Components/Router.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {BrowserRouter as Router, Route, Redirect, Switch} from 'react-router-dom';
+import Home from "Routes/Home";
+import TV from "Routes/TV";
+import Search from "Routes/Search";
+
+export default () => (
+    <Router>
+        <Switch>
+            {/* exact: 정확히 해당 path여야 함 */}
+            <Route path="/" exact component={Home} />
+            <Route path="/tv" exact component={TV} />
+            <Route path="/tv/popular" component={() => <h1>Popular</h1>} />
+            <Route path="/search" component={Search} />
+            <Redirect from="*" to="/" />
+        </Switch>
+    </Router>
+)
